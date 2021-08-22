003f83c6d42d0160d99fd92f5c794b063ad748f5
Eduardo Dall Gallo <edallgallo@gmail.copm>
2021-08-22 20:45:19 -0300

    Connecting To Database

---
+++ 04_RestWhitASPNETUdemy_ConnectingToDatabase/RestWhitASPNETUdemy/RestWhitASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,20 @@
+﻿using RestWhitASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWhitASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindByID(long id);
+
+        List<Person> FindAll();
+        
+        Person Update(Person person);
+        
+        void Delete(long id);
+
+
+    }
+}
