926805b0704d894142c175fb86c2a671fcf27903
Eduardo Dall Gallo <edallgallo@gmail.copm>
2021-09-07 20:08:36 -0300

    Working With Generic Repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/BookBusinessIMplementation.cs
@@ -0,0 +1,40 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class BookBusinessIMplementation: IBookBusinnes
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookBusinessIMplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+        public Book FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+        public Book Update(Book book)
+        {
+            return _repository.Update(book); ;
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+
+    }
+}
