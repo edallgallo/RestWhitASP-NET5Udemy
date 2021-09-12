9161173924fd0249070a95e885401d1a31d779b0
Eduardo Dall Gallo <edallgallo@gmail.copm>
2021-09-12 11:32:51 -0300

    Working With Value Objects

---
+++ 09_RestWithASPNETUdemy_WorkingWithValueObjects/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Book : BaseEntity
+    {
+
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("author")]
+        public string Author { get; set; }
+        [Column("launch_date")]
+        public DateTime LauchDate {get; set; }
+        [Column("price")]
+        public Decimal  Price { get; set; }
+    }
+}
