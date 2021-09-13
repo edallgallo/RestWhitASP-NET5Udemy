0169345b3fd8798a4cb3c862f76c1ac669e881ce
Eduardo Dall Gallo <edallgallo@gmail.copm>
2021-09-13 07:37:19 -0300

    Custom Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime LauchDate { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
