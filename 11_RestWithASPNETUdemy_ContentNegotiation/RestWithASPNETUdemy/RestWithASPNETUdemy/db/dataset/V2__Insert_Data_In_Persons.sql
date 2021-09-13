dc4fc64d6bc014f734dfc37f20ad65e180caeb77
Eduardo Dall Gallo <edallgallo@gmail.copm>
2021-09-13 08:36:19 -0300

    Working with Content Negotiation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Insert_Data_In_Persons.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Airton', 'Male', 'Senna'),
+	(2, 'Anchiano Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(3, 'Porbandar India', 'Maradma', 'Male', 'Gandi'),
+	(4, 'Mvzo South Africa', 'Nelson', 'Male', 'Mandela');
